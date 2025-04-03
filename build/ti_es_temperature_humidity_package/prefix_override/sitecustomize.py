ad72ac8e2e5a80d81d0c67a626f8c7de1604bd61
DavidAkerboom <1056357@hr.nl>
2025-04-03 12:33:17 +0200

    Removed build, install, and log folders & updated .gitignore

--- build/ti_es_temperature_humidity_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/david/tinlabs_project/src/ti_es_temperature_humidity_package/install/ti_es_temperature_humidity_package'
