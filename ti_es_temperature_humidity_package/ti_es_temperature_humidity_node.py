d7d1c0d0d90f4d9b5dc6e22cd1de86e57404ca1a
DavidAkerboom <1056357@hr.nl>
2025-04-14 23:01:05 +0200

    Removed unnecessary change in main

--- ti_es_temperature_humidity_package/ti_es_temperature_humidity_node.py
+++ ti_es_temperature_humidity_package/ti_es_temperature_humidity_node.py
@@ -1,7 +1,6 @@
 import smbus
 import lgpio
 import rclpy
-# from rclpy.node import Node
 from rclpy.lifecycle import Node, State, TransitionCallbackReturn
 import time
 from enum import Enum
@@ -26,7 +25,11 @@ class temperatureHumidityNode(Node):
     def __init__(self,**kwargs):
         super().__init__('temperature_humidity_topic',**kwargs)
 
+
     def on_configure(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info("Configuring...")
+
+        # Initialize publishers, services, or one-time setup here
         time.sleep(0.1)
         while True:
             try:
@@ -39,32 +42,43 @@ class temperatureHumidityNode(Node):
 
         self.log_publisher = self.create_publisher(String, "ti/es/log_data", 10)        
 
-        self.get_logger().info('on_configure() is called.')
         return TransitionCallbackReturn.SUCCESS
+
 
     def on_activate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info("Activating...")
+        
+        # Start timers or subscriptions
         self.timer = self.create_timer(I2C_TIMER, self.timer_callback)
 
-        self.get_logger().info('on_activate() is called.')
         return super().on_activate(state)
 
+
     def on_deactivate(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info("Deactivating...")
+
+        # Stop timers or other activity
         self.destroy_timer(self.timer_callback)
 
-        self.get_logger().info('on_deactivate() is called.')
         return super().on_deactivate(state)
 
+
     def on_cleanup(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info("Cleaning up...")
+
+        # Destroy publishers or clean resources
         self.destroy_publisher(self.log_publisher)
 
-        self.get_logger().info('on_cleanup() is called.')
         return TransitionCallbackReturn.SUCCESS
 
+
     def on_shutdown(self, state: State) -> TransitionCallbackReturn:
+        self.get_logger().info("Shutting down...")
+
+        # Final cleanup if needed
         self.destroy_timer(self.timer_callback)
         self.destroy_publisher(self.log_publisher)
 
-        self.get_logger().info('on_shutdown() is called.')
         return TransitionCallbackReturn.SUCCESS
 
 
@@ -107,12 +121,10 @@ class temperatureHumidityNode(Node):
 def main(args=None):
     rclpy.init(args=args)
 
-    executor = rclpy.executors.SingleThreadedExecutor()
     thn = temperatureHumidityNode()
-    executor.add_node(thn)
     print('Temperature/humidity Package has booted!')
     try:
-        executor.spin()
+        rclpy.spin(thn)
     except (KeyboardInterrupt, rclpy.executors.ExternalShutdownException):
         pass
     finally:
